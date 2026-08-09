01610c598379281c7d0c33087fd71521d398aeda
Michał Fritza <fritza.michal@proton.me>
2026-08-09 23:33:15 +0200

    Terraform layout.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,21 @@
 terraform {
-	required_providers {
+  required_providers {
     azurerm = {
-			source = "hashicorp/azurerm"
-			version = "5.0.1"
+      source  = "hashicorp/azurerm"
+      version = "~> 5.0"
     }
+  }
+  backend "azurerm" {
+    use_azuread_auth     = true
+    resource_group_name  = "rg-azuregitops-tfstate"
+    storage_account_name = "aksgitops"
+    container_name       = "tfstate"
+    key                  = "gitops-lab/platform.tfstate"
   }
 }
 
 provider "azurerm" {
-	features {}
-	subscription_id = var.subscription_id
+  features {}
+  subscription_id = var.subscription_id
+  resource_provider_registrations = "core"
 }
