237d3b74f1f61ee8e90b8368ddf49a62bf4e3e42
Michał Fritza <fritza.michal@proton.me>
2026-08-12 23:30:42 +0200

    Made repo public, changes repository source to https, fix to overlay patches.

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,8 +35,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_role_assignment" "arc_image_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
